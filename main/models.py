e0cb1ec119e821242bfd732cf72c2a0fb128c580
sebbby <seb.beitel@gmail.com>
2020-11-20 15:00:07 -0500

    complete assessment

--- main/models.py
+++ main/models.py
@@ -1,4 +1,7 @@
-# from django.db import models
+from django.db import models
 
-# class Task(models.Model):
+class Task(models.Model):
+  description = models.CharField(max_length=100)
+  time = models.IntegerField()
+  person = models.CharField(max_length=100)
 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from .models import Task
+from .forms import TaskForm
 
 def index(request):
-  return render(request, 'index.html')
+  tasks = Task.objects.all()
+  if request.method == 'POST':
+    form = TaskForm(request.POST)
+    if form.is_valid():
+      new_task = form.save()
+      return HttpResponseRedirect('/')
+  else:
+    form = TaskForm()
+  return render(request, 'index.html', {'form':form, 'tasks':tasks })
+
+def delete_task(request, task_id):
+  Task.objects.get(pk=task_id).delete()
+  return redirect('index')
+
+
+
+
+
