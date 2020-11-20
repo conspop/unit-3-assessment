bc8bbde925026ee116280ccd3f7cdc97bf3e286a
sebbby <seb.beitel@gmail.com>
2020-11-20 15:04:56 -0500

    complete bonus

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('delete_task/<int:task_id>', views.delete_task, name='delete_task')
+    path('delete_task/<int:task_id>', views.delete_task, name='delete_task'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from .models import Task
 from .forms import TaskForm
+from django.db.models import Sum
 
 def index(request):
   tasks = Task.objects.all()
+  total_time = tasks.aggregate(Sum('time'))
   if request.method == 'POST':
     form = TaskForm(request.POST)
     if form.is_valid():
@@ -12,7 +14,7 @@ def index(request):
       return HttpResponseRedirect('/')
   else:
     form = TaskForm()
-  return render(request, 'index.html', {'form':form, 'tasks':tasks })
+  return render(request, 'index.html', {'form':form, 'tasks':tasks, 'total_time': total_time })
 
 def delete_task(request, task_id):
   Task.objects.get(pk=task_id).delete()
